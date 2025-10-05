eb9169378ee7d1bbaefe442d97852ad8831d1960
JessHaDig <hallo@digitalagentur-hansmann.de>
2025-10-05 17:35:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, 'client'),
+  root: './client',
   build: {
-    outDir: path.resolve(__dirname, 'dist/public'),
+    outDir: '../dist/public',
     emptyOutDir: true
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client/src'),
-      '@shared': path.resolve(__dirname, 'shared')
+      '@': path.resolve(__dirname, './client/src'),
+      '@shared': path.resolve(__dirname, './shared')
     }
   }
 });
